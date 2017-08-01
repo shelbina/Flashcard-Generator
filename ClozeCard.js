a85c392683120a4d399b3672f7b21fc0eaa7c8ff
shelbina <deel.shelby@gmail.comgit clone git@github.com:shelbina/mayeleven.gitgit@github.com:shelbina/mayeleven.git$ git config --global user.email deel.shelby@gmail.com>
2017-08-01 18:19:24 -0400

    add pages

---
+++ ClozeCard.js
@@ -0,0 +1,8 @@
+// define a Node module that exports a constructor for creating cloze-deletion flashcards, e.g.: module.exports = ClozeCard;
+// constructor should accept two arguments: text and cloze.
+// constructed object should have a cloze property that contains only the cloze-deleted portion of the text.
+// constructed object should have a partial property that contains only the partial text.
+// constructed object should have a fullText property that contains only the full text.
+// constructor should throw or log an error when the cloze deletion does not appear in the input text.
+// Use prototypes to attach these methods, wherever possible.
+// bulk of this assignment is implementing ClozeCard. If you build a robust ClozeCard implementation, feel free to try your hand at implementing a front-end, as well.
