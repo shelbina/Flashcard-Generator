cf563743566f4d8555651b4dbc49969453f95816
shelbina <deel.shelby@gmail.comgit clone git@github.com:shelbina/mayeleven.gitgit@github.com:shelbina/mayeleven.git$ git config --global user.email deel.shelby@gmail.com>
2017-08-03 16:17:44 -0400

    update based on directions

--- BasicCard.js
+++ BasicCard.js
@@ -1,4 +1,9 @@
-// This file should define a Node module that exports a constructor for creating basic flashcards, e.g.: module.exports = BasicCard;
+// This file should define a Node module that exports a constructor for creating basic flashcards, e.g.:
+module.exports = BasicCard; 
 // The constructor should accept two arguments: front and back.
-// The constructed object should have a front property that contains the text on the front of the card.
-// The constructed object should have a back property that contains the text on the back of the card.
+function BasicCard(front, Back){
+ // The constructed object should have a front property that contains the text on the front of the card.   
+    this.front = front;
+// The constructed object should have a back property that contains the text on the back of the card.    
+    this.back = back;
+};
--- ClozeCard.js
+++ ClozeCard.js
@@ -1,8 +1,24 @@
-// define a Node module that exports a constructor for creating cloze-deletion flashcards, e.g.: module.exports = ClozeCard;
+var BasicCard = require("./BasicCard.js");
+// define a Node module that exports a constructor for creating cloze-deletion flashcards, e.g.:
+//module.exports = ClozeCard; 
 // constructor should accept two arguments: text and cloze.
-// constructed object should have a cloze property that contains only the cloze-deleted portion of the text.
-// constructed object should have a partial property that contains only the partial text.
-// constructed object should have a fullText property that contains only the full text.
+var firstPresident = new BasicCard(
+    "Who was the first president of the United States?", "George Washington");
+// "Who was the first president of the United States?"
+console.log(firstPresident.front); 
+// "George Washington"
+console.log(firstPresident.back);
+
+var firstPresidentCloze = new ClozeCard(
+    "George Washington was the first president of the United States.", "George Washington");
+// "George Washington" cloze property that contains only the cloze-deleted portion of the text.
+console.log(firstPresidentCloze.cloze); 
+// " ... was the first president of the United States. partial property that contains only the partial text.
+console.log(firstPresidentCloze.partial);
+// "George Washington was the first president of the United States. fullText property that contains only the full text.
+console.log(firstPresidentCloze.fullText)
+
 // constructor should throw or log an error when the cloze deletion does not appear in the input text.
+var brokenCloze = new ClozeCard("This doesn't work", "oops");
+
 // Use prototypes to attach these methods, wherever possible.
-// bulk of this assignment is implementing ClozeCard. If you build a robust ClozeCard implementation, feel free to try your hand at implementing a front-end, as well.
