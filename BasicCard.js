17394a5b853603784bc741856276073e794a8e16
shelbina <deel.shelby@gmail.comgit clone git@github.com:shelbina/mayeleven.gitgit@github.com:shelbina/mayeleven.git$ git config --global user.email deel.shelby@gmail.com>
2017-08-04 00:15:00 -0400

    update javascript.js prompts

--- BasicCard.js
+++ BasicCard.js
@@ -1,9 +1,25 @@
-// This file should define a Node module that exports a constructor for creating basic flashcards, e.g.:
-module.exports = BasicCard; 
+var fs = require("fs");
+var inquirer = require("inquirer");
+
 // The constructor should accept two arguments: front and back.
 function BasicCard(front, Back){
- // The constructed object should have a front property that contains the text on the front of the card.   
-    this.front = front;
-// The constructed object should have a back property that contains the text on the back of the card.    
+    this.front = front;  
     this.back = back;
-};
+	this.create = function() {
+       
+        var data = {
+            front: this.front,
+            back: this.back,
+            type: "basic",
+        };
+
+        fs.appendFile("log.txt", JSON.stringify(data) + ';', "utf8", function(error) {
+          
+            if (error) {
+                console.log(error);
+            }
+        });
+    };
+}
+// This file should define a Node module that exports a constructor for creating basic flashcards, e.g.:
+module.exports = BasicCard;
--- ClozeCard.js
+++ ClozeCard.js
@@ -1,24 +1,24 @@
 var BasicCard = require("./BasicCard.js");
+var fs = require("fs");
+
+function ClozeCard(text, Cloze){
+    this.text = text.split(cloze);
+    this.cloze = cloze;
+        this.create = function() {
+        var data = {
+            text: this.text,
+            cloze: this.cloze,
+            clozeDeleted: this.clozeDeleted,
+            type: "cloze"
+        };
+        fs.appendFile("log.txt", JSON.stringify(data) + ';', "utf8", function(error) {
+// constructor should throw or log an error when the cloze deletion does not appear in the input text.          
+            if (error) {
+                console.log(error);
+            }
+        });
+    };
+}
+
 // define a Node module that exports a constructor for creating cloze-deletion flashcards, e.g.:
-//module.exports = ClozeCard; 
-// constructor should accept two arguments: text and cloze.
-var firstPresident = new BasicCard(
-    "Who was the first president of the United States?", "George Washington");
-// "Who was the first president of the United States?"
-console.log(firstPresident.front); 
-// "George Washington"
-console.log(firstPresident.back);
-
-var firstPresidentCloze = new ClozeCard(
-    "George Washington was the first president of the United States.", "George Washington");
-// "George Washington" cloze property that contains only the cloze-deleted portion of the text.
-console.log(firstPresidentCloze.cloze); 
-// " ... was the first president of the United States. partial property that contains only the partial text.
-console.log(firstPresidentCloze.partial);
-// "George Washington was the first president of the United States. fullText property that contains only the full text.
-console.log(firstPresidentCloze.fullText)
-
-// constructor should throw or log an error when the cloze deletion does not appear in the input text.
-var brokenCloze = new ClozeCard("This doesn't work", "oops");
-
-// Use prototypes to attach these methods, wherever possible.
+module.exports = ClozeCard;
